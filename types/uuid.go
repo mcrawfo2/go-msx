02f515566a14df08bb791099aecb135f32caa570
Michael Crawford <mcrawfo2@cisco.com>
2019-11-05 14:03:00 -0500

    Make UUID json-compatible

--- types/uuid.go
+++ types/uuid.go
@@ -2,10 +2,29 @@ package types
 
 import (
 	"bytes"
+	"encoding/json"
 	"github.com/hashicorp/go-uuid"
 )
 
 type UUID []byte
+
+func (u UUID) MarshalJSON() ([]byte, error) {
+	str, err := uuid.FormatUUID(u)
+	return []byte(str), err
+}
+
+func (u UUID) UnmarshalJSON(data []byte) error {
+	var uuidString string
+	if err := json.Unmarshal(data, &uuidString); err != nil {
+		return err
+	}
+	if uuidBytes, err := uuid.ParseUUID(uuidString); err != nil {
+		return err
+	} else {
+		copy(u, uuidBytes)
+	}
+	return nil
+}
 
 func ParseUUID(value string) (UUID, error) {
 	return uuid.ParseUUID(value)
