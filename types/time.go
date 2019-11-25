78dcf45013471edc318ce18c27981977bfbf4c46
Michael Crawford <mcrawfo2@cisco.com>
2019-11-24 20:45:54 -0500

    Add JSON marshalable Time type

---
+++ types/time.go
@@ -0,0 +1,29 @@
+package types
+
+import (
+	"encoding/json"
+	"time"
+)
+
+const timeLayout = "2006-01-02T15:04:05.999999999Z"
+
+type Time time.Time
+
+func (t Time) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Time(t).Format(timeLayout))
+}
+
+func (t *Time) UnmarshalJSON(data []byte) error {
+	var str *string
+	err := json.Unmarshal(data, &str)
+	if err != nil {
+		return err
+	}
+	var v time.Time
+	v, err = time.Parse(timeLayout, *str)
+	if err != nil {
+		return err
+	}
+	*t = Time(v)
+	return nil
+}
