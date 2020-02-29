8c8eef3777cf87a86a90d46d61ad8493cca861c7
Michael Crawford <mcrawfo2@cisco.com>
2020-02-29 17:09:54 -0500

    Extend optional string
    - Provide another optional string if nil
    - Constructor

--- types/optional.go
+++ types/optional.go
@@ -8,6 +8,13 @@ func (s OptionalString) IsPresent() bool {
 	return s.Value != nil
 }
 
+func (s OptionalString) OrOptional(value *string) OptionalString {
+	if s.Value == nil {
+		return OptionalString{value}
+	}
+	return s
+}
+
 func (s OptionalString) OrElse(value string) string {
 	if s.Value != nil {
 		return *s.Value
@@ -17,4 +24,8 @@ func (s OptionalString) OrElse(value string) string {
 
 func (s OptionalString) String() string {
 	return s.OrElse("<nil>")
+}
+
+func NewOptionalString(value *string) OptionalString {
+	return OptionalString{Value:value}
 }
