f11f9f66cc09d96cde06a8ef62358be05938cab2
Michael Crawford <mcrawfo2@cisco.com>
2020-03-17 10:56:50 -0400

    Lucene index generator Mk 2

--- cassandra/lucene/index.go
+++ cassandra/lucene/index.go
@@ -1,7 +1,8 @@
 package lucene
 
 import (
-	"cto-github.cisco.com/NFV-BU/go-msx/cassandra/ddl"
+	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -19,16 +20,86 @@ const (
 	IndexOptionSchema              = "schema"
 )
 
+type IndexOptions struct {
+	RefreshSeconds      *int
+	DirectoryPath       *string
+	RamBufferMb         *string
+	MaxMergeMb          *string
+	MaxCachedMb         *string
+	IndexingThreads     *int
+	IndexingQueuesSize  *string
+	ExcludedDataCenters *string
+	Partitioner         *string
+	Sparse              *bool
+	Schema              *IndexSchema
+}
+
+func (o IndexOptions) optionStrings() []string {
+	var result []string
+	result = append(result, o.Option(IndexOptionRefreshSeconds, o.RefreshSeconds))
+	result = append(result, o.Option(IndexOptionDirectoryPath, o.DirectoryPath))
+	result = append(result, o.Option(IndexOptionRamBufferMb, o.RamBufferMb))
+	result = append(result, o.Option(IndexOptionMaxMergeMb, o.MaxMergeMb))
+	result = append(result, o.Option(IndexOptionMaxCachedMb, o.MaxCachedMb))
+	result = append(result, o.Option(IndexOptionIndexingThreads, o.IndexingThreads))
+	result = append(result, o.Option(IndexOptionIndexingQueuesSize, o.IndexingQueuesSize))
+	result = append(result, o.Option(IndexOptionExcludedDataCenters, o.ExcludedDataCenters))
+	result = append(result, o.Option(IndexOptionPartitioner, o.Partitioner))
+	result = append(result, o.Option(IndexOptionSparse, o.Sparse))
+	result = append(result, o.Option(IndexOptionSchema, o.Schema))
+	return result
+}
+
+func (o IndexOptions) String() string {
+	sb := new(strings.Builder)
+	sb.WriteString("{")
+	out := false
+	for _, v := range o.optionStrings() {
+		if v == "" {
+			continue
+		}
+		if out {
+			sb.WriteRune(',')
+		}
+		sb.WriteString(v)
+		out = true
+	}
+	sb.WriteRune('}')
+
+	return sb.String()
+}
+
+func (o IndexOptions) Option(key string, value interface{}) string {
+	if value == nil {
+		return ""
+	}
+
+	valueString := ""
+	if valueStringer, ok := value.(fmt.Stringer); ok {
+		valueString = valueStringer.String()
+	} else if valueInt, ok := value.(*int); ok {
+		valueString = strconv.Itoa(*valueInt)
+	} else if valueBool, ok := value.(*bool); ok {
+		if *valueBool {
+			valueString = "true"
+		} else {
+			valueString = "false"
+		}
+	} else {
+		valueString = fmt.Sprintf("%v", value)
+	}
+
+	return fmt.Sprintf(`'%s':'%s'`, key, valueString)
+}
+
 type Index struct {
 	Name    string
 	Table   string
 	Column  *string
-	Options map[string]string
+	Options IndexOptions
 }
 
-type IndexQueryBuilder struct {
-	o ddl.OptionsQueryPartBuilder
-}
+type IndexQueryBuilder struct{}
 
 func (b *IndexQueryBuilder) CreateIndex(i Index) string {
 	sb := new(strings.Builder)
@@ -42,6 +113,16 @@ func (b *IndexQueryBuilder) CreateIndex(i Index) string {
 	}
 	sb.WriteRune(')')
 	sb.WriteString("USING 'com.stratio.cassandra.lucene.index' WITH OPTIONS = ")
-	sb.WriteString(b.o.Options(i.Options))
+	sb.WriteString(i.Options.String())
+	return sb.String()
+}
+
+func (b *IndexQueryBuilder) DropIndex(i Index, ifExists bool) string {
+	sb := new(strings.Builder)
+	sb.WriteString("DROP INDEX ")
+	if ifExists {
+		sb.WriteString("IF EXISTS ")
+	}
+	sb.WriteString(i.Name)
 	return sb.String()
 }
---
+++ cassandra/lucene/schema.go
@@ -0,0 +1,67 @@
+package lucene
+
+import (
+	"encoding/json"
+	"strings"
+)
+
+const (
+	DataTypeString  = "string"
+	DataTypeUuid    = "uuid"
+	DataTypeInteger = "integer"
+	DataTypeDate    = "date"
+	DataTypeText    = "text"
+	DataTypeBoolean = "boolean"
+
+	OptionPattern       = "pattern"
+	OptionCaseSensitive = "case_sensitive"
+)
+
+type IndexSchema []IndexSchemaField
+
+type IndexSchemaField struct {
+	Name     string
+	DataType string
+	Options  FieldOptions
+}
+
+type FieldOptions map[string]interface{}
+
+func (s *IndexSchema) WithFieldOptions(name string, dataType string, options FieldOptions) *IndexSchema {
+	field := IndexSchemaField{
+		Name:     name,
+		DataType: dataType,
+		Options:  options,
+	}
+	*s = append(*s, field)
+	return s
+}
+
+func (s *IndexSchema) WithField(name string, dataType string) *IndexSchema {
+	return s.WithFieldOptions(name, dataType, FieldOptions{})
+}
+
+func (s *IndexSchema) String() string {
+	sb := new(strings.Builder)
+
+	for i, field := range *s {
+		if i > 0 {
+			sb.WriteRune(',')
+		}
+		sb.WriteRune('"')
+		sb.WriteString(field.Name)
+		sb.WriteString(`":{"type":"`)
+		sb.WriteString(field.DataType)
+		sb.WriteRune('"')
+		for optionKey, optionValue := range field.Options {
+			sb.WriteString(`,"`)
+			sb.WriteString(optionKey)
+			sb.WriteString(`":`)
+			bytes, _ := json.Marshal(optionValue)
+			sb.Write(bytes)
+		}
+		sb.WriteRune('}')
+	}
+
+	return sb.String()
+}
