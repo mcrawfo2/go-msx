d631819a91a9d0cceb8377f39cab3b6fe5c8f15a
Michael Crawford <mcrawfo2@cisco.com>
2020-05-17 10:11:41 -0400

    Extend local tenant access check

--- rbac/tenant.go
+++ rbac/tenant.go
@@ -14,12 +14,25 @@ var ErrUserDoesNotHaveTenantAccess = errors.New("User does not have access to th
 func HasTenant(ctx context.Context, tenantId types.UUID) error {
 	logger.WithContext(ctx).Debugf("Verifying tenant access for tenantId %q", tenantId.String())
 
+	if allTenants, err := HasAccessAllTenants(ctx); err != nil {
+		return err
+	} else if allTenants {
+		return nil
+	}
+
+	userContext := security.UserContextFromContext(ctx)
+	if userContext.TenantId.Equals(tenantId) {
+		return nil
+	}
+
 	userContextDetails, err := security.NewUserContextDetails(ctx)
 	if err != nil {
 		return err
 	}
 
 	if !userContextDetails.HasTenantId(tenantId) {
+		// TODO: Check remotely
+
 		logger.WithContext(ctx).
 			WithError(ErrUserDoesNotHaveTenantAccess).
 			Errorf("Tenant access check failed for tenantId %q", tenantId.String())
