2dc4593d2c72bb5642a534d3228363ab5318d30e
Michael Crawford <mcrawfo2@cisco.com>
2020-05-26 15:27:02 -0400

    Add StringQueue type

---
+++ types/string_queue.go
@@ -0,0 +1,34 @@
+package types
+
+import "container/list"
+
+type StringQueue struct {
+	*list.List
+}
+
+func (s StringQueue) Contains(value string) bool {
+	for e := s.Front(); e != nil; e = e.Next(){
+		if e.Value.(string) == value {
+			return true
+		}
+	}
+	return false
+}
+
+func (s StringQueue) Push(value string) {
+	s.PushBack(value)
+}
+
+func (s StringQueue) Pop() string {
+	return s.Remove(s.Front()).(string)
+}
+
+func (s StringQueue) Peek() string {
+	return s.Front().Value.(string)
+}
+
+func NewStringQueue() StringQueue {
+	return StringQueue{
+		list.New(),
+	}
+}
