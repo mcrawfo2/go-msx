270af742315aa9afd62167b4c72e1b5c27f060e0
Andrei <anpop@cisco.com>
2020-06-04 12:31:34 -0400

    PR feedback

--- webservice/swaggerprovider/provider.go
+++ webservice/swaggerprovider/provider.go
@@ -220,12 +220,9 @@ func RegisterSwaggerProvider(ctx context.Context) error {
 				Title: "MSX API Documentation for " + appInfo.Name,
 				Description: "<h3>This is the REST API documentation for " + appInfo.Name + "</h3>\n \n" +
 					appInfo.Description + "\n" +
-					"" +
 					"+ API Authorization \n" +
 					"    + Authorization header is <b>required</b>. \n" +
-					"    + It should be in Bearer authentication scheme </br>(e.g <b> Authorization: BEARER &lt;access token&gt; </b>)\n" +
-					"" +
-					"",
+					"    + It should be in Bearer authentication scheme </br>(e.g <b> Authorization: BEARER &lt;access token&gt; </b>)\n",
 				TermsOfService: "http://www.cisco.com",
 				Contact: &spec.ContactInfo{
 					Name:  "Cisco Systems Inc.",
