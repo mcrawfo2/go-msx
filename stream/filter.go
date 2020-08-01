81bc2f6a9f21e574c56b54050b82115c14fb70c3
Michael Crawford <mcrawfo2@cisco.com>
2020-08-01 18:54:02 -0400

    Add SERVICECONFIG_UPDATE_TOPIC streaming
    - Listeners
      - Message filters
    - Publisher

---
+++ stream/filter.go
@@ -0,0 +1,30 @@
+package stream
+
+import (
+	"context"
+	"cto-github.cisco.com/NFV-BU/go-msx/types"
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+type MessageFilter func(ctx context.Context, metadata message.Metadata) bool
+
+func FilterByMetaData(key string, values... string) MessageFilter {
+	return func(ctx context.Context, metadata message.Metadata) bool {
+		metaDataValue := metadata[key]
+		if !types.StringStack(values).Contains(metaDataValue) {
+			logger.WithContext(ctx).Infof("Filtering out message with %s %q", key, metaDataValue)
+			return false
+		}
+
+		return true
+	}
+}
+
+func FilterMessage(msg *message.Message, filters []MessageFilter) bool {
+	for _, filter := range filters {
+		if !filter(msg.Context(), msg.Metadata) {
+			return false
+		}
+	}
+	return true
+}
