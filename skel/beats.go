92a3381fa426ed294ee085f2eb010f8f8f4fb7fa
Michael Crawford <mcrawfo2@cisco.com>
2020-09-12 16:25:38 -0400

    Unify template renderer

--- skel/beats.go
+++ skel/beats.go
@@ -34,57 +34,45 @@ func GenerateBeatsDomain(args []string) error {
 	metaPackageSource := path.Join("code", "beats", "_meta")
 	metaPackagePath := path.Join("internal", "_meta")
 
-	files := []domainDefinitionFile{
+	templates := TemplateSet{
 		{
-			Name: inflections[inflectionAppTitle] + " Field Descriptors",
-			Template: Template{
-				SourceFile: path.Join(metaPackageSource, "fields.yml"),
-				DestFile:   path.Join(metaPackagePath, "fields.yml"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Field Descriptors",
+			SourceFile: path.Join(metaPackageSource, "fields.yml"),
+			DestFile:   path.Join(metaPackagePath, "fields.yml"),
+			Format:     FileFormatYaml,
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " DTO",
-			Template: Template{
-				SourceFile: path.Join(apiPackageSource, "device.go"),
-				DestFile:   path.Join(apiPackagePath, "device.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " DTO",
+			SourceFile: path.Join(apiPackageSource, "device.go"),
+			DestFile:   path.Join(apiPackagePath, "device.go"),
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Beater Init",
-			Template: Template{
-				SourceFile: path.Join(beaterPackageSource, "init.go.tpl"),
-				DestFile:   path.Join(beaterPackagePath, "init.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Beater Init",
+			SourceFile: path.Join(beaterPackageSource, "init.go.tpl"),
+			DestFile:   path.Join(beaterPackagePath, "init.go"),
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Beater Config",
-			Template: Template{
-				SourceFile: path.Join(beaterPackageSource, "config.go.tpl"),
-				DestFile:   path.Join(beaterPackagePath, "config.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Beater Config",
+			SourceFile: path.Join(beaterPackageSource, "config.go.tpl"),
+			DestFile:   path.Join(beaterPackagePath, "config.go"),
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Beater State",
-			Template: Template{
-				SourceFile: path.Join(beaterPackageSource, "state.go.tpl"),
-				DestFile:   path.Join(beaterPackagePath, "state.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Beater State",
+			SourceFile: path.Join(beaterPackageSource, "state.go.tpl"),
+			DestFile:   path.Join(beaterPackagePath, "state.go"),
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Beater Implementation",
-			Template: Template{
-				SourceFile: path.Join(beaterPackageSource, "beater.go.tpl"),
-				DestFile:   path.Join(beaterPackagePath, "beater.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Beater Implementation",
+			SourceFile: path.Join(beaterPackageSource, "beater.go.tpl"),
+			DestFile:   path.Join(beaterPackagePath, "beater.go"),
 		},
 	}
 
-	packagePaths := map[string]string{
-		"cto-github.cisco.com/NFV-BU/go-msx/skel/templates/code/beats/api": apiPackageUrl,
-	}
+	options := NewRenderOptions()
+	options.AddStrings(inflections)
+	options.AddString("cto-github.cisco.com/NFV-BU/go-msx/skel/templates/code/beats/api", apiPackageUrl)
 
-	err := renderDomain(files, inflections, nil, packagePaths)
-	if err != nil {
+	if err := templates.Render(options); err != nil {
 		return err
 	}
 
--- skel/sp.go
+++ skel/sp.go
@@ -24,71 +24,56 @@ func GenerateServicePack(args []string) error {
 	slmPackageSource := path.Join("code", "sp", "platform-common", "servicelifecycle")
 	slmPackagePath := path.Join("platform-common", "servicelifecycle")
 
-	files := []domainDefinitionFile{
+	templates := TemplateSet{
 		{
-			Name: inflections[inflectionAppTitle] + " Subscription DTO",
-			Template: Template{
-				SourceFile: path.Join(apiPackageSource, "subscription.go"),
-				DestFile:   path.Join(apiPackagePath, "subscription.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Subscription DTO",
+			SourceFile: path.Join(apiPackageSource, "subscription.go"),
+			DestFile:   path.Join(apiPackagePath, "subscription.go"),
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Subscription Context",
-			Template: Template{
-				SourceFile: path.Join(subscriptionPackageSource, "context.go.tpl"),
-				DestFile:   path.Join(subscriptionPackagePath, "context.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Subscription Context",
+			SourceFile: path.Join(subscriptionPackageSource, "context.go.tpl"),
+			DestFile:   path.Join(subscriptionPackagePath, "context.go"),
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Subscription Controller",
-			Template: Template{
-				SourceFile: path.Join(subscriptionPackageSource, "controller.go.tpl"),
-				DestFile:   path.Join(subscriptionPackagePath, "controller.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Subscription Controller",
+			SourceFile: path.Join(subscriptionPackageSource, "controller.go.tpl"),
+			DestFile:   path.Join(subscriptionPackagePath, "controller.go"),
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Subscription Controller",
-			Template: Template{
-				SourceFile: path.Join(subscriptionPackageSource, "converter.go.tpl"),
-				DestFile:   path.Join(subscriptionPackagePath, "converter.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Subscription Controller",
+			SourceFile: path.Join(subscriptionPackageSource, "converter.go.tpl"),
+			DestFile:   path.Join(subscriptionPackagePath, "converter.go"),
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Subscription Model",
-			Template: Template{
-				SourceFile: path.Join(subscriptionPackageSource, "model.go.tpl"),
-				DestFile:   path.Join(subscriptionPackagePath, "model.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Subscription Model",
+			SourceFile: path.Join(subscriptionPackageSource, "model.go.tpl"),
+			DestFile:   path.Join(subscriptionPackagePath, "model.go"),
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Subscription Service",
-			Template: Template{
-				SourceFile: path.Join(subscriptionPackageSource, "service.go.tpl"),
-				DestFile:   path.Join(subscriptionPackagePath, "service.go"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Subscription Service",
+			SourceFile: path.Join(subscriptionPackageSource, "service.go.tpl"),
+			DestFile:   path.Join(subscriptionPackagePath, "service.go"),
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Service Lifecycle Manifest",
-			Template: Template{
-				SourceFile: path.Join(slmPackageSource, "manifest.json"),
-				DestFile:   path.Join(slmPackagePath, "manifest.json"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Service Lifecycle Manifest",
+			SourceFile: path.Join(slmPackageSource, "manifest.json"),
+			DestFile:   path.Join(slmPackagePath, "manifest.json"),
+			Format:     FileFormatJson,
 		},
 		{
-			Name: inflections[inflectionAppTitle] + " Service Lifecycle Deployment Manifest",
-			Template: Template{
-				SourceFile: path.Join(slmPackageSource, "manifest.yml"),
-				DestFile:   path.Join(slmPackagePath, "manifest.yml"),
-			},
+			Name:       inflections[inflectionAppTitle] + " Service Lifecycle Deployment Manifest",
+			SourceFile: path.Join(slmPackageSource, "manifest.yml"),
+			DestFile:   path.Join(slmPackagePath, "manifest.yml"),
+			Format:     FileFormatYaml,
 		},
 	}
 
-	packagePaths := map[string]string{
-		"cto-github.cisco.com/NFV-BU/go-msx/skel/templates/code/sp/api": apiPackageUrl,
-	}
+	options := NewRenderOptions()
+	options.AddStrings(inflections)
+	options.AddString("cto-github.cisco.com/NFV-BU/go-msx/skel/templates/code/sp/api", apiPackageUrl)
 
-	err := renderDomain(files, inflections, nil, packagePaths)
-	if err != nil {
+	if err := templates.Render(options); err != nil {
 		return err
 	}
 
