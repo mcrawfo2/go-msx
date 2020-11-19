25f17742c05797c403cc4065021360d96c229ff0
Michael Crawford <mcrawfo2@cisco.com>
2020-11-19 15:28:29 -0500

    Add status change topic support

---
+++ stream/topics/statuschange/types.go
@@ -0,0 +1,21 @@
+package statuschange
+
+const (
+	StateCreated              = "created"
+	StateUpdated              = "updated"
+	StateDeleted              = "deleted"
+	StateProvisioning         = "provisioning"
+	StateProvisioned          = "provisioned"
+	StateProvisioningFailed   = "provisioningFailed"
+	StateDeprovisioning       = "deprovisioning"
+	StateDeprovisioned        = "deprovisioned"
+	StateDeprovisioningFailed = "deprovisioningFailed"
+
+	TopicStatusChange = "STATUS_CHANGE_TOPIC"
+)
+
+type Message struct {
+	EntityType string `json:"entityType"`
+	EntityId   string `json:"entityId"`
+	Status     string `json:"status"`
+}
