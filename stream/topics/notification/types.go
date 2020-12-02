dac3e903cf3caffb06ca0e7695eddc7530e5b92a
harvinder singh <harvinds@cisco.com>
2020-12-02 09:19:34 -0800

    pr comments

--- stream/topics/notification/types.go
+++ stream/topics/notification/types.go
@@ -3,10 +3,8 @@ package notification
 import (
 	"context"
 	"errors"
-	"time"
 
 	"cto-github.cisco.com/NFV-BU/go-msx/security"
-	"cto-github.cisco.com/NFV-BU/go-msx/stream/topics"
 )
 
 type Message struct {
@@ -36,93 +34,6 @@ type ServiceType struct {
 	DisplayNameResourceKey string `json:"diplayNameResourceKey"`
 }
 
-//PayloadMessage : its the payload struct used by the notification service to unmarshal the Kafka.Message into
-//if the payload field you need to add does not exist in the following payload struct please go ahead and add it.
-//PayloadMessage refers to : Message.Payload for notification-go templates.
-type PayloadMessage struct {
-	User struct {
-		ID        string `json:"id"`
-		Email     string `json:"email"`
-		UserID    string `json:"userId"`
-		Name      string `json:"name"`
-		FirstName string `json:"firstName"`
-		LastName  string `json:"lastName"`
-		Locale    string `json:"locale"`
-	} `json:"user"`
-
-	Alert struct {
-		Time        string `json:"time"`
-		Severity    string `json:"severity"`
-		Type        string `json:"type"`
-		Subtype     string `json:"subtype"`
-		Service     string `json:"service"`
-		Description string `json:"description"`
-	}
-
-	Tenant struct {
-		ID           string      `json:"id"`
-		Name         string      `json:"name"`
-		DisplayName  string      `json:"displayname"`
-		ProviderName string      `json:"providername"`
-		Description  string      `json:"description"`
-		Url          string      `json:"url"`
-		GroupName    string      `json:"groupname"`
-		Suspended    interface{} `json:"suspended"`
-	} `json:"tenant"`
-
-	Provider struct {
-		ID               string `json:"id"`
-		Email            string `json:"email"`
-		Name             string `json:"name"`
-		NotificationType string `json:"notificationType"`
-		Locale           string `json:"locale"`
-		DisplayName      string `json:"displayname"`
-	} `json:"provider"`
-
-	RestDetails   interface{} `json:"restDetails"`
-	StatusMessage string      `json:"statusMessage"`
-	Users         []struct {
-		ID        string `json:"id"`
-		Email     string `json:"email"`
-		UserID    string `json:"userId"`
-		FirstName string `json:"firstName"`
-		LastName  string `json:"lastName"`
-		Roles     []struct {
-			Name        string `json:"name"`
-			DisplayName string `json:"displayName"`
-		} `json:"roles"`
-		Tenants []struct {
-			Name        string `json:"name"`
-			DisplayName string `json:"displayName"`
-		} `json:"tenants"`
-		Status           string `json:"status"`
-		IsDeleted        bool   `json:"isDeleted"`
-		ProviderNameDesc string `json:"providerNameDesc"`
-		ExpireDays       int    `json:"expireDays"`
-		GraceLogins      int    `json:"graceLogins"`
-		URL              string `json:"url"`
-		TokenTimeoutMins string `json:"token_timeout_mins"`
-	} `json:"users"`
-
-	Service struct {
-		ServiceID string `json:"serviceId"`
-	} `json:"service,omitempty"`
-
-	Devices []struct {
-		ID    string `json:"id"`
-		Name  string `json:"name"`
-		SN    string `json:"sn"`
-		Model string `json:"model"`
-		Type  string `json:"type"`
-	} `json:"devices"`
-
-	Address struct {
-		DisplayName string `json:"displayname"`
-	} `json:"address"`
-
-	DisplayName string `json:"displayname"`
-}
-
 type MessageProducer interface {
 	Message(context.Context) (Message, error)
 }
@@ -136,18 +47,17 @@ func NewMessage(ctx context.Context) (Message, error) {
 	}
 
 	return Message{
-		Timestamp: topics.Time(time.Now().UTC()),
 		Context: Context{
 			User: Identifier{
-				Id:   userContextDetails.UserId,
+				Id:   userContextDetails.UserId.String(),
 				Name: *userContextDetails.Username,
 			},
 			Provider: Identifier{
-				Id:   userContextDetails.ProviderId,
+				Id:   userContextDetails.ProviderId.String(),
 				Name: *userContextDetails.ProviderName,
 			},
 			Tenant: Identifier{
-				Id:   userContextDetails.TenantId,
+				Id:   userContextDetails.TenantId.String(),
 				Name: *userContextDetails.TenantName,
 			},
 		},
