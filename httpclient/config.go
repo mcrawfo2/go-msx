edf3ed0917d12cd50c07842f9e4147655f1c9e64
Michael Crawford <mcrawfo2@cisco.com>
2021-02-04 11:54:57 -0500

    185: Configure httpclient with secure default ciphers and tls versions in production

--- httpclient/config.go
+++ httpclient/config.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"cto-github.cisco.com/NFV-BU/go-msx/config"
+	"fmt"
 	"github.com/pkg/errors"
 	"io/ioutil"
 	"time"
@@ -12,12 +13,14 @@ import (
 const configRootHttpClient = "http.client"
 
 type ClientConfig struct {
-	Timeout     time.Duration `config:"default=30s"`
-	IdleTimeout time.Duration `config:"default=1s"`
-	TlsInsecure bool          `config:"default=true"`
-	LocalCaFile string        `config:"default="`
-	CertFile    string        `config:"default="`
-	KeyFile     string        `config:"default="`
+	Timeout         time.Duration `config:"default=30s"`
+	IdleTimeout     time.Duration `config:"default=1s"`
+	LocalCaFile     string        `config:"default="`
+	CertFile        string        `config:"default="`
+	KeyFile         string        `config:"default="`
+	TlsInsecure     bool          `config:"default=true"`
+	TlsMinVersion   string        `config:"default=tls10"`
+	TlsCipherSuites []string      `config:"default="`
 }
 
 func NewClientConfig(cfg *config.Config) (*ClientConfig, error) {
@@ -83,10 +86,17 @@ func NewTlsConfig(clientConfig *ClientConfig) (*tls.Config, error) {
 		return nil, err
 	}
 
+	ciphers, err := ParseCiphers(clientConfig.TlsCipherSuites)
+	if err != nil {
+		return nil, err
+	}
+
 	tlsConfig := &tls.Config{
 		InsecureSkipVerify: clientConfig.TlsInsecure,
 		RootCAs:            rootCAs,
 		Certificates:       clientCerts,
+		MinVersion:         TLSLookup[clientConfig.TlsMinVersion],
+		CipherSuites:       ciphers,
 	}
 
 	if len(clientCerts) > 0 {
@@ -94,4 +104,60 @@ func NewTlsConfig(clientConfig *ClientConfig) (*tls.Config, error) {
 	}
 
 	return tlsConfig, nil
+}
+
+// ParseCiphers parse ciphersuites from the comma-separated string into
+// recognized slice
+func ParseCiphers(ciphers []string) ([]uint16, error) {
+	var suites []uint16
+
+	for _, cipher := range ciphers {
+		if v, ok := CipherLookup[cipher]; ok {
+			suites = append(suites, v)
+		} else {
+			return nil, fmt.Errorf("unsupported cipher %q", cipher)
+		}
+	}
+
+	return suites, nil
+}
+
+// CipherLookup maps the cipher names to the internal value
+var CipherLookup = map[string]uint16{
+	"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305":          tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+	"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305":        tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+	"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256":         tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256":       tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384":         tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384":       tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+	"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256":         tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+	"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA":            tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+	"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256":       tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+	"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA":          tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+	"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA":            tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+	"TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA":          tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+	"TLS_RSA_WITH_AES_128_GCM_SHA256":               tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+	"TLS_RSA_WITH_AES_256_GCM_SHA384":               tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+	"TLS_RSA_WITH_AES_128_CBC_SHA256":               tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
+	"TLS_RSA_WITH_AES_128_CBC_SHA":                  tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+	"TLS_RSA_WITH_AES_256_CBC_SHA":                  tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+	"TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA":           tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+	"TLS_RSA_WITH_3DES_EDE_CBC_SHA":                 tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+	"TLS_RSA_WITH_RC4_128_SHA":                      tls.TLS_RSA_WITH_RC4_128_SHA,
+	"TLS_ECDHE_RSA_WITH_RC4_128_SHA":                tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+	"TLS_ECDHE_ECDSA_WITH_RC4_128_SHA":              tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
+	"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256":   tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+	"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256": tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+	"TLS_AES_128_GCM_SHA256":                        tls.TLS_AES_128_GCM_SHA256,
+	"TLS_AES_256_GCM_SHA384":                        tls.TLS_AES_256_GCM_SHA384,
+	"TLS_CHACHA20_POLY1305_SHA256":                  tls.TLS_CHACHA20_POLY1305_SHA256,
+}
+
+// TLSLookup maps the tls.MinVersion configuration to the internal value
+var TLSLookup = map[string]uint16{
+	"":      tls.VersionTLS10, // default in golang
+	"tls10": tls.VersionTLS10,
+	"tls11": tls.VersionTLS11,
+	"tls12": tls.VersionTLS12,
+	"tls13": tls.VersionTLS13,
 }
