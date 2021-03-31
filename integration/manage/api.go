6d53ddf9f23a4389caf26889ccd7bec897d02846
Scott Lehman <scolehma@cisco.com>
2021-03-31 14:21:57 -0700

    Add/update deprecations of some usermanagement and manage integrations

--- integration/manage/api.go
+++ integration/manage/api.go
@@ -21,27 +21,46 @@ type Api interface {
 	// CreateServiceOrder
 	// UpdateServiceOrder
 
+	// Deprecated: Use v8/ServicesApiService (or newer)
 	GetServiceInstance(serviceInstanceId string) (*integration.MsxResponse, error)
+	// Deprecated: REST API was deprecated in 3.10.0
 	GetSubscriptionServiceInstances(subscriptionId string, page, pageSize int) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/ServicesApiService (or newer)
 	CreateServiceInstance(subscriptionId, serviceInstanceId string, serviceAttribute, serviceDefAttribute, status map[string]string) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/ServicesApiService (or newer)
 	UpdateServiceInstance(serviceInstanceId string, serviceAttribute, serviceDefAttribute, status map[string]string) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/ServicesApiService (or newer)
 	DeleteServiceInstance(serviceInstanceId string) (*integration.MsxResponse, error)
 
+	// Deprecated: Use v8/SiteApiService (or newer)
 	GetSitesV3(siteFilters SiteQueryFilter, page, pageSize int) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/SiteApiService (or newer)
 	GetSiteV3(siteId string, showImage string) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/SiteApiService (or newer)
 	CreateSiteV3(siteRequest SiteCreateRequest) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/SiteApiService (or newer)
 	UpdateSiteV3(siteRequest SiteUpdateRequest, siteId string, notification string) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/SiteApiService (or newer)
 	DeleteSiteV3(siteId string) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/SiteApiService (or newer)
 	AddDeviceToSiteV3(deviceId string, siteId string, notification string) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/SiteApiService (or newer)
 	DeleteDeviceFromSiteV3(deviceId string, siteId string) (*integration.MsxResponse, error)
+	// Deprecated: No replacement planned (site status will be calculated only)
 	UpdateSiteStatusV3(siteStatus SiteStatusUpdateRequest, siteId string) (*integration.MsxResponse, error)
 
+	// Deprecated: Use v8/DevicesApiService (or newer)
 	GetDeviceConfig(deviceInstanceId string) (*integration.MsxResponse, error)
 
+	// Deprecated: Use v8/DevicesApiService (or newer)
 	CreateDeviceV4(deviceRequest DeviceCreateRequest) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/DevicesApiService (or newer)
 	DeleteDeviceV4(deviceId string, force string) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/DevicesApiService (or newer)
 	GetDevicesV4(requestQuery map[string][]string, page int, pageSize int) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/DevicesApiService (or newer)
 	GetDeviceV4(deviceId string) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/DevicesApiService (or newer)
 	UpdateDeviceV4(deviceRequest DeviceUpdateRequest, deviceId string) (*integration.MsxResponse, error)
 	UpdateDeviceStatusV4(deviceStatus DeviceStatusUpdateRequest, deviceId string) (*integration.MsxResponse, error)
 
@@ -50,10 +69,15 @@ type Api interface {
 	AddDeviceTemplate(deviceTemplateCreateRequest DeviceTemplateCreateRequest) (*integration.MsxResponse, error)
 	DeleteDeviceTemplate(templateId types.UUID) (*integration.MsxResponse, error)
 
+	// Deprecated: Use v8/DevicesApiService (or newer)
 	GetDeviceTemplateHistory(deviceInstanceId string) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/DevicesApiService (or newer)
 	AttachDeviceTemplates(deviceId string, attachTemplateRequest AttachTemplateRequest) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/DevicesApiService (or newer)
 	DetachDeviceTemplates(deviceId string) (*integration.MsxResponse, error)
+	// Deprecated: Use v8/DevicesApiService (or newer)
 	DetachDeviceTemplate(deviceId string, templateId types.UUID) (*integration.MsxResponse, error)
+
 	UpdateTemplateAccess(templateId string, deviceTemplateAccess DeviceTemplateAccess) (*integration.MsxResponse, error)
 
 	CreateDeviceActions(deviceActionList DeviceActionCreateRequests) (*integration.MsxResponse, error)
