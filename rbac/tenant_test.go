19379237ee20362e92f39f47e855e7295b341cea
Scott Lehman <scolehma@cisco.com>
2022-03-02 18:36:41 -0800

    HasTenant and HasAccessToTenant also make a tenant hierarchy check.
    Using the hierarchy as a fall back will grant access to tenants that are created
    during a user's session, after the context is loaded.

--- rbac/tenant_test.go
+++ rbac/tenant_test.go
@@ -2,26 +2,38 @@ package rbac
 
 import (
 	"context"
+	"testing"
+
 	"cto-github.cisco.com/NFV-BU/go-msx/integration"
 	"cto-github.cisco.com/NFV-BU/go-msx/integration/usermanagement"
 	"cto-github.cisco.com/NFV-BU/go-msx/types"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestHasTenant_Explicit(t *testing.T) {
 	tenantId, _ := types.NewUUID()
 	tenantBadId, _ := types.NewUUID()
-	ctx := context.Background()
+
+	userManagementApi := new(usermanagement.MockUserManagement)
+	ctx := usermanagement.ContextWithIntegration(context.Background(), userManagementApi)
 	setMockTokenDetailsProvider(ctx, []types.UUID{tenantId}, []string{})
+	mockedUsermanagement := usermanagement.IntegrationFromContext(ctx).(*usermanagement.MockUserManagement)
 
 	var err error
 	err = HasTenant(ctx, tenantId)
 	assert.NoError(t, err)
 
+	mockedUsermanagement.On("GetTenantHierarchyAncestors", tenantBadId).Return(nil, []types.UUID{}, nil)
 	err = HasTenant(ctx, tenantBadId)
 	assert.Error(t, err)
 	assert.Equal(t, ErrUserDoesNotHaveTenantAccess, err)
+
+	rootTenantId, _ := types.NewUUID()
+	childTenantId, _ := types.NewUUID()
+	ancestors := []types.UUID{rootTenantId, tenantId}
+	mockedUsermanagement.On("GetTenantHierarchyAncestors", childTenantId).Return(nil, ancestors, nil)
+	err = HasAccessToTenant(ctx, childTenantId)
+	assert.NoError(t, err)
 }
 
 func TestHasTenant_Implicit(t *testing.T) {
@@ -35,44 +47,21 @@ func TestHasTenant_Implicit(t *testing.T) {
 }
 
 func TestHasAccessToTenant(t *testing.T) {
-
 	tenantId, _ := types.NewUUID()
-	tenantBadId, _ := types.NewUUID()
-
-	//mock responses
 	rootTenantId, _ := types.NewUUID()
-	parentTenantId, _ := types.NewUUID()
-
-	rootTenantIdMsxResponse := &integration.MsxResponse{
-		StatusCode: 200,
-		BodyString: rootTenantId.String(),
-	}
-
-	parentTenantIdMsxResponse := &integration.MsxResponse{
-		StatusCode: 200,
-		BodyString: parentTenantId.String(),
-	}
-
-	badTenantIdMsxResponse := &integration.MsxResponse{
-		StatusCode: 200,
-		BodyString: "",
-	}
 
 	//build mocked context
-	ctx := context.Background()
 	userManagementApi := new(usermanagement.MockUserManagement)
-	ctx = usermanagement.ContextWithIntegration(context.Background(), userManagementApi)
+	ctx := usermanagement.ContextWithIntegration(context.Background(), userManagementApi)
 	setMockTokenDetailsProvider(ctx, []types.UUID{tenantId}, []string{})
-
 	mockedUsermanagement := usermanagement.IntegrationFromContext(ctx).(*usermanagement.MockUserManagement)
-	mockedUsermanagement.On("GetTenantHierarchyRoot").Return(rootTenantIdMsxResponse, nil).Maybe()
-	mockedUsermanagement.On("GetTenantHierarchyParent", tenantId).Return(parentTenantIdMsxResponse, nil).Maybe()
 
 	//validate
 	err := HasAccessToTenant(ctx, tenantId)
 	assert.NoError(t, err)
 
-	mockedUsermanagement.On("GetTenantHierarchyParent", tenantBadId).Return(badTenantIdMsxResponse, nil).Maybe()
+	tenantBadId, _ := types.NewUUID()
+	mockedUsermanagement.On("GetTenantHierarchyAncestors", tenantBadId).Return(nil, []types.UUID{rootTenantId}, nil)
 	notValid := HasAccessToTenant(ctx, tenantBadId)
 	assert.Error(t, notValid, ErrTenantDoesNotExist)
 
