87b576eba08872667b3ba626f602ed9764eed478
harvinds <harvinds@cisco.com>
2022-05-05 13:29:10 -0400

    check response for nil

--- vault/connection_impl.go
+++ vault/connection_impl.go
@@ -364,10 +364,16 @@ func (c connectionImpl) GetTransitKeys(ctx context.Context) (results []string, e
 	if err != nil {
 		err = errors.Wrap(err, "Failed to get transit keys")
 	}
-	keys := secrets.Data["keys"].([]interface{})
-	for _, key := range keys {
-		results = append(results, fmt.Sprintf("%v", key))
+
+	if secrets != nil {
+		keys, ok := secrets.Data["keys"].([]interface{})
+		if ok {
+			for _, key := range keys {
+				results = append(results, fmt.Sprintf("%v", key))
+			}
+		}
 	}
+
 	return results, err
 }
 
