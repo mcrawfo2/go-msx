e1a7cfe49fc0424f5a0c28ef22cbb0c9412308d4
Ana <ananasta@cisco.com>
2022-06-10 11:10:27 -0400

    Add type handling for arrays

--- types/merge.go
+++ types/merge.go
@@ -15,7 +15,21 @@ func (m Merge) RecursiveMerge(src map[string]interface{}, dest map[string]interf
 			dest[key] = m.RecursiveMerge(src[key].(map[string]interface{}), dest[key].(map[string]interface{}))
 		} else if dest[key] != nil && reflect.TypeOf(value).Kind() == reflect.Slice && reflect.TypeOf(dest[key]).Kind() == reflect.Slice {
 			// append the two arrays
-			dest[key] = append(dest[key].([]interface{}), src[key].([]interface{}))
+			switch dest[key].(type) {
+			case []interface{}:
+				dest[key] = append(dest[key].([]interface{}), value.([]interface{})...)
+			case []string:
+				dest[key] = append(dest[key].([]string), value.([]string)...)
+			case []int:
+				dest[key] = append(dest[key].([]int), value.([]int)...)
+			case []bool:
+				dest[key] = append(dest[key].([]bool), value.([]bool)...)
+			case []float32:
+				dest[key] = append(dest[key].([]float32), value.([]float32)...)
+			case []float64:
+				dest[key] = append(dest[key].([]float64), value.([]float64)...)
+			}
+
 		} else {
 			dest[key] = value
 		}
--- types/merge_test.go
+++ types/merge_test.go
@@ -146,3 +146,70 @@ func TestRecursiveMerge_DestSrcEmpty(t *testing.T) {
 	expectedResult := make(map[string]interface{})
 	assert.Equal(t, expectedResult, result)
 }
+
+// 8. append arrays - simple types
+func TestRecursiveMerge_DestSrcAppendArraysBasic(t *testing.T) {
+	dest := make(map[string]interface{})
+	dest["key1"] = "key1val"
+	dest["intarray"] = []int{1, 2, 3}
+	dest["stringarray"] = []string{"hello"}
+	dest["float32array"] = []float32{10.00}
+	dest["float64array"] = []float64{10.00}
+	dest["boolarray"] = []bool{false}
+
+	src := make(map[string]interface{})
+	src["key1"] = "key1val"
+	src["intarray"] = []int{4, 5, 6}
+	src["stringarray"] = []string{"world"}
+	src["float32array"] = []float32{10.00}
+	src["float64array"] = []float64{10.00}
+	src["boolarray"] = []bool{true}
+	merge := Merge{}
+	result := merge.RecursiveMerge(src, dest)
+
+	expectedResult := make(map[string]interface{})
+	expectedResult["key1"] = "key1val"
+	expectedResult["intarray"] = []int{1, 2, 3, 4, 5, 6}
+	expectedResult["stringarray"] = []string{"hello", "world"}
+	expectedResult["float32array"] = []float32{10.00, 10.00}
+	expectedResult["float64array"] = []float64{10.00, 10.00}
+	expectedResult["boolarray"] = []bool{false, true}
+
+	assert.Equal(t, expectedResult, result)
+}
+
+// 9. append arrays - complex types
+func TestRecursiveMerge_DestSrcAppendArraysComplex(t *testing.T) {
+	dest := make(map[string]interface{})
+	dest["key1"] = "key1val"
+	dest["complextype"] = []interface{}{
+		map[string]interface{}{
+			"key1": "1",
+			"key2": "2",
+			"key3": "3",
+		}}
+
+	src := make(map[string]interface{})
+	src["key1"] = "key1val"
+	src["complextype"] = []interface{}{
+		map[string]interface{}{
+			"name": "namevalue",
+		}}
+
+	merge := Merge{}
+	result := merge.RecursiveMerge(src, dest)
+
+	expectedResult := make(map[string]interface{})
+	expectedResult["key1"] = "key1val"
+	expectedResult["complextype"] = []interface{}{
+		map[string]interface{}{
+			"key1": "1",
+			"key2": "2",
+			"key3": "3",
+		},
+		map[string]interface{}{
+			"name": "namevalue",
+		}}
+
+	assert.Equal(t, expectedResult, result)
+}
