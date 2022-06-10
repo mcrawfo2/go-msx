70b9763ea7ce2eeb44a9d5cd9fd7c38a526d0401
Ana <ananasta@cisco.com>
2022-06-09 21:44:34 -0400

    Add merge for types; enabling nested merging

---
+++ types/merge.go
@@ -0,0 +1,26 @@
+package types
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Merge struct{}
+
+// This is only necessary until go is upgrade to version 18, where "copy" functionality and generics become available
+func (m Merge) RecursiveMerge(src map[string]interface{}, dest map[string]interface{}) map[string]interface{} {
+	for key, value := range src {
+		if dest[key] == nil {
+			fmt.Printf("Setting the dest key %s to: %+v", key, value)
+			dest[key] = value
+		} else if dest[key] != nil && reflect.TypeOf(value).Kind() == reflect.Map {
+			dest[key] = m.RecursiveMerge(src[key].(map[string]interface{}), dest[key].(map[string]interface{}))
+		} else if dest[key] != nil && reflect.TypeOf(value).Kind() == reflect.Slice && reflect.TypeOf(dest[key]).Kind() == reflect.Slice {
+			// append the two arrays
+			dest[key] = append(dest[key].([]interface{}), src[key].([]interface{}))
+		} else {
+			dest[key] = value
+		}
+	}
+	return dest
+}
---
+++ types/merge_test.go
@@ -0,0 +1,148 @@
+package types
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// 1. dest is empty, source is not
+func TestRecursiveMerge_DestBaseSrcEmpty(t *testing.T) {
+	src := make(map[string]interface{})
+	src["key1"] = "key1val"
+	src["key2"] = "key2val"
+
+	dest := make(map[string]interface{})
+	merge := Merge{}
+	result := merge.RecursiveMerge(src, dest)
+
+	expectedResult := make(map[string]interface{})
+	expectedResult["key1"] = "key1val"
+	expectedResult["key2"] = "key2val"
+	assert.Equal(t, expectedResult, result)
+}
+
+// 2. source is empty, dest is not - done
+func TestRecursiveMerge_DestEmptySrcBase(t *testing.T) {
+	dest := make(map[string]interface{})
+	dest["key1"] = "key1val"
+	dest["key2"] = "key2val"
+
+	src := make(map[string]interface{})
+	merge := Merge{}
+	result := merge.RecursiveMerge(src, dest)
+
+	expectedResult := make(map[string]interface{})
+	expectedResult["key1"] = "key1val"
+	expectedResult["key2"] = "key2val"
+	assert.Equal(t, expectedResult, result)
+}
+
+// 3. src, dest are all basic values
+func TestRecursiveMerge_DestBasicSrcBasic(t *testing.T) {
+	dest := make(map[string]interface{})
+	dest["key1"] = "key1val"
+	dest["key2"] = "key2val"
+
+	src := make(map[string]interface{})
+	src["key3"] = 123
+	src["key2"] = 120.012
+	merge := Merge{}
+	result := merge.RecursiveMerge(src, dest)
+
+	expectedResult := make(map[string]interface{})
+	expectedResult["key1"] = "key1val"
+	expectedResult["key2"] = 120.012
+	expectedResult["key3"] = 123
+	assert.Equal(t, expectedResult, result)
+}
+
+// 4. dest complex value gets replaced with a simple src value
+func TestRecursiveMerge_DestInterfaceSrcBasic(t *testing.T) {
+	dest := make(map[string]interface{})
+	dest["key1"] = map[string]interface{}{
+		"name":  123,
+		"value": "value",
+	}
+	dest["key2"] = "key2val"
+
+	src := make(map[string]interface{})
+	src["key3"] = 123
+	src["key2"] = "key2val"
+	src["key1"] = "testvalue-basictype"
+	merge := Merge{}
+	result := merge.RecursiveMerge(src, dest)
+
+	expectedResult := make(map[string]interface{})
+	expectedResult["key1"] = "testvalue-basictype"
+	expectedResult["key2"] = "key2val"
+	expectedResult["key3"] = 123
+	assert.Equal(t, expectedResult, result)
+}
+
+// 5. nested object: only one of the child keys gets updated
+func TestRecursiveMerge_DestInterfaceSrcInterface(t *testing.T) {
+	dest := make(map[string]interface{})
+	dest["key1"] = map[string]interface{}{
+		"name":  123,
+		"value": "value",
+	}
+	dest["key2"] = "key2val"
+
+	src := make(map[string]interface{})
+	src["key3"] = 123
+	src["key2"] = "key2val"
+	src["key1"] = map[string]interface{}{
+		"name": 4567,
+	}
+	merge := Merge{}
+	result := merge.RecursiveMerge(src, dest)
+
+	expectedResult := make(map[string]interface{})
+	expectedResult["key1"] = map[string]interface{}{
+		"name":  4567,
+		"value": "value",
+	}
+	expectedResult["key2"] = "key2val"
+	expectedResult["key3"] = 123
+	assert.Equal(t, expectedResult, result)
+}
+
+// 6. src and destination are identical
+func TestRecursiveMerge_DestSrcIdentical(t *testing.T) {
+	dest := make(map[string]interface{})
+	dest["key1"] = map[string]interface{}{
+		"name":  123,
+		"value": "value",
+	}
+	dest["key2"] = "key2val"
+
+	src := make(map[string]interface{})
+	src["key1"] = map[string]interface{}{
+		"name":  123,
+		"value": "value",
+	}
+	src["key2"] = "key2val"
+
+	merge := Merge{}
+	result := merge.RecursiveMerge(src, dest)
+
+	expectedResult := make(map[string]interface{})
+	expectedResult["key1"] = map[string]interface{}{
+		"name":  123,
+		"value": "value",
+	}
+	expectedResult["key2"] = "key2val"
+	assert.Equal(t, expectedResult, result)
+}
+
+// 7. src and destination are empty
+func TestRecursiveMerge_DestSrcEmpty(t *testing.T) {
+	dest := make(map[string]interface{})
+	src := make(map[string]interface{})
+
+	merge := Merge{}
+	result := merge.RecursiveMerge(src, dest)
+
+	expectedResult := make(map[string]interface{})
+	assert.Equal(t, expectedResult, result)
+}
