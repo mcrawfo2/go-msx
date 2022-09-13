096bd1c11f8c5f81a9e0eb317c4189c03ba73d22
Michael Crawford <mcrawfo2@cisco.com>
2022-09-13 07:34:32 -0400

    Fix checkErr in service/repository test helpers

--- testhelpers/error.go
+++ testhelpers/error.go
@@ -5,6 +5,7 @@
 package testhelpers
 
 import (
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -13,4 +14,30 @@ func ReportErrors(t *testing.T, name string, errs []error) {
 	for _, err := range errs {
 		assert.Fail(t, err.Error(), "Failed %s validator", name)
 	}
+}
+
+func CheckErr(got []interface{}, hasErr, wantErr bool) error {
+	if wantErr {
+		if len(got) == 0 {
+			return errors.Errorf("Wanted Error, No values returned")
+		} else if !hasErr {
+			return errors.Errorf("Wanted Error, Method not flagged as returning error")
+		} else if nil == got[len(got)-1] {
+			return errors.Errorf("Wanted Error, No error returned")
+		} else if _, ok := got[len(got)-1].(error); !ok {
+			return errors.Errorf("Wanted Error, Non-error value returned")
+		}
+	} else if hasErr {
+		if len(got) == 0 {
+			return errors.Errorf("Method flagged as returning error, No values returned")
+		} else if err, ok := got[len(got)-1].(error); ok && err != nil {
+			return errors.Errorf("Unwanted Error, Error returned:\n%s", Dump(err))
+		} else if nil == got[len(got)-1] {
+			// No error returned
+		} else if _, ok := got[len(got)-1].(error); !ok {
+			return errors.Errorf("Non-error value returned")
+		}
+	}
+
+	return nil
 }
