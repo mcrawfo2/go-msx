84c1ec9437fd1c55b06529d38fa152d7fabe1aaf
Michael Crawford <mcrawfo2@cisco.com>
2022-11-22 13:08:26 -0500

    Separate parent span context propagation into trace module

--- trace/context.go
+++ trace/context.go
@@ -8,7 +8,10 @@ import "context"
 
 type contextKey string
 
-const contextKeySpan = contextKey("Span")
+const (
+	contextKeySpan       = contextKey("Span")
+	contextKeyParentSpan = contextKey("ParentSpan")
+)
 
 func SpanFromContext(ctx context.Context) Span {
 	span, _ := ctx.Value(contextKeySpan).(Span)
@@ -17,4 +20,13 @@ func SpanFromContext(ctx context.Context) Span {
 
 func ContextWithSpan(ctx context.Context, span Span) context.Context {
 	return context.WithValue(ctx, contextKeySpan, span)
+}
+
+func ParentContextFromContext(ctx context.Context) SpanContext {
+	spanContext, _ := ctx.Value(contextKeyParentSpan).(SpanContext)
+	return spanContext
+}
+
+func ContextWithParentContext(ctx context.Context, spanContext SpanContext) context.Context {
+	return context.WithValue(ctx, contextKeyParentSpan, spanContext)
 }
