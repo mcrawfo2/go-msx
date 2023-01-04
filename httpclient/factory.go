1c9f4172af6301ff0820036156d337690612457d
Michael Crawford <mcrawfo2@cisco.com>
2023-01-04 07:32:27 -0500

    Enable http.Client to be used as httpclient.Client

--- httpclient/factory.go
+++ httpclient/factory.go
@@ -23,6 +23,12 @@ type Client interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
+type ClientFunc func(req *http.Request) (*http.Response, error)
+
+func (d ClientFunc) Do(req *http.Request) (*http.Response, error) {
+	return d(req)
+}
+
 type DoFunc func(req *http.Request) (*http.Response, error)
 
 func (d DoFunc) RoundTrip(request *http.Request) (*http.Response, error) {
